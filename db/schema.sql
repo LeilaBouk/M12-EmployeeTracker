8bd983ca60a36f46a5fe67e4b4692ee9a6ec36c9
Leila <leila.boukella@gmail.com>
2022-10-17 17:13:59 -0700

    added required info to schema

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    dept_id INT,
+    FOREIGN KEY (dept_id)
+    REFRENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+        FOREIGN KEY (role_id)
+    REFRENCES role(id)
+    ON DELETE SET NULL
+        FOREIGN KEY (manager_id)
+    REFRENCES employee(id)
+    ON DELETE SET NULL
+)
